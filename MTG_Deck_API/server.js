c2df093297db73bf4b7a4f4ab80ab5aed2718dc5
Jake Hawkinson <jakehawk1095@gmail.com>
2017-04-08 17:14:07 -0700

    basic server.js structure complete

--- MTG_Deck_API/server.js
+++ MTG_Deck_API/server.js
@@ -1,1 +1,25 @@
-var express
+var express 		= require('express'),
+		app 				= express(),
+		path				= require('path'),
+		cors 				= require('cors'),
+		logger 			= require('morgan'),
+		mongoose 		= require('mongoose'),
+		bodyParser 	= require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/mtg-stuff-1');
+
+var routes = require('./config/routes');
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(routes);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, 
+	()=> console.log(`Live on port ${port}`)
+);
