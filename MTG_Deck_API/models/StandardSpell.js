12d518fafceb4e1edcdadf36bda1f9b8a19e71bc
Jake Hawkinson <jakehawk1095@gmail.com>
2017-04-09 12:42:05 -0700

    fixed WUBRGC to be required and default to 0

--- MTG_Deck_API/models/StandardSpell.js
+++ MTG_Deck_API/models/StandardSpell.js
@@ -4,12 +4,12 @@ var StandardSpellSchema = mongoose.Schema({
 	name 		: String,
 	cmc 		: Number,
 	colors  : [String], 
-	w			 	: Number,
-	u  			: Number,
-	b 			: Number,
-	r		 		: Number,
-	g				: Number,
-	c 			: Number,
+	w			 	: { type: Number, required: true, default: 0},
+	u  			: { type: Number, required: true, default: 0},
+	b 			: { type: Number, required: true, default: 0},
+	r		 		: { type: Number, required: true, default: 0},
+	g				: { type: Number, required: true, default: 0},
+	c 			: { type: Number, required: true, default: 0},
 	img_url : String
 });
 
