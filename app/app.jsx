1e1ab2781241a1f0b57c429d2e5bf3e598806437
Jake Hawkinson <jakehawk1095@gmail.com>
2017-04-12 11:54:14 -0700

    updating front end with updated react boilerplate

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,14 @@
-var React 		= require('react'),
-		ReactDOM 	= require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-	<h1>MTG Deck Builder</h1>,
-	document.getElementById('app')
+  <p>Boilerplate 3 Project</p>,
+  document.getElementById('app')
 );
